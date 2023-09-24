4a95b9c3df826b5ef9c4eda76260223c24e0e453
shridhiaggarwal <shridhi.aggarwal@gmail.com>
2023-09-24 17:08:53 +0530

    added header

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+// JavaScript to change the header box shadow after scrolling past 100vh
+// window.addEventListener('scroll', function() {
+//     const header = document.querySelector('header');
+//     if (window.scrollY >= 200) {
+//         header.style.boxShadow = '0 10px 30px -10px #020c1bb3';
+//     } else {
+//         header.style.boxShadow = 'none';
+//     }
+// });
