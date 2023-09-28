4d34c055fd16f6f9b1f2f4188b7b531e69516a2f
shridhiaggarwal <shridhi.aggarwal@gmail.com>
2023-09-28 19:26:41 +0530

    added scroll to top button

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,4 @@
-// JavaScript to change the header box shadow after scrolling past 100vh
-// window.addEventListener('scroll', function() {
-//     const header = document.querySelector('header');
-//     if (window.scrollY >= 200) {
-//         header.style.boxShadow = '0 10px 30px -10px #020c1bb3';
-//     } else {
-//         header.style.boxShadow = 'none';
-//     }
-// });
-
+//show more projects and less projects function
 document.addEventListener("DOMContentLoaded", function () {
     const showHideButton = document.getElementById("showHideButton");
     const hiddenProjects = document.querySelectorAll(".projectBox.hidden");
@@ -32,3 +23,33 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add a click event listener to the "Show More" button
     showHideButton.addEventListener("click", toggleHiddenProjects);
 });
+
+
+// Get the scroll-to-top button element
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+
+// JavaScript to change the header box shadow after scrolling past 100vh
+// window.addEventListener('scroll', function() {
+//     const header = document.querySelector('header');
+//     if (window.scrollY >= 200) {
+//         header.style.boxShadow = '0 10px 30px -10px #020c1bb3';
+//     } else {
+//         header.style.boxShadow = 'none';
+//     }
+// });
